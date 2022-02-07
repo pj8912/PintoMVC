1c673b27ab134feedc8fbe23dbcb1480a23a64b7
pj8912 <jpintofun2b@gmail.com>
2022-02-08 02:22:37 +0530

    mysql check

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 #PintoMVC
 A small web framework created using PHP based on MVC pattern
 </h1>
+<?php
+
+$conn = mysqli_connect('localhost','lookfnbi_jp98', '16uph306','lookfnbi_testing');
+if($conn){
+	echo 'mysql:working';
+}
+else{
+	echo 'fuck!';
+}
 
