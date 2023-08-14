a0e4373827e05c26bbc0a5102aa526637113a714
John Pinto <59218902+pj8912@users.noreply.github.com>
2023-08-14 20:42:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,3 @@
 #PintoMVC
 A small web framework created using PHP based on MVC pattern
 </h1>
-<?php
-
-$conn = mysqli_connect('localhost','lookfnbi_jp98', '16uph306','lookfnbi_testing');
-if($conn){
-	echo 'mysql:working';
-}
-else{
-	echo 'fuck!';
-}
-
